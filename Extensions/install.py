565e8b4ef9ec4ed6dbc514ccca8dda9e937fca7f
madarche <devnull@localhost>
2005-03-18 16:06:52 +0000

    Initial revision

---
+++ Extensions/install.py
@@ -0,0 +1,52 @@
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author:
+# M.-A. Darche <madarche@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+
+from zLOG import LOG, INFO, DEBUG
+
+# XXX I think that as long as you don't run any CPSSpecific methods,
+# this should still work under pure CMF. This needs to be tested though.
+from Products.CPSInstaller.CPSInstaller import CPSInstaller
+
+class ProductInstaller(CPSInstaller):
+    pass
+
+def install(self):
+
+    installer = ProductInstaller(self, 'CPSRemoteController')
+    installer.log("Starting CPSRemoteController install")
+
+##     skins = {
+##         'cps_schemas': 'Products/CPSRemoteController/skins/cps_schemas',
+##         'cps_jscalendar': 'Products/CPSRemoteController/skins/cps_jscalendar',
+##     }
+##     installer.verifySkins(skins)
+
+    # Tools
+    installer.verifyTool('portal_remote_controller', 'CPSRemoteController',
+                         'CPS Remote Controller Tool')
+
+    # importing .po files
+    # Non CPS installation may not have Localizer.
+##     if installer.portalHas('Localizer'):
+##         installer.setupTranslations()
+
+    installer.finalize()
+    installer.log("End of specific CPSRemoteController install")
+    return installer.logResult()
---
+++ RemoteControllerTool.py
@@ -0,0 +1,217 @@
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Author:
+# M.-A. Darche <madarche@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""
+"""
+
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.permissions import ManagePortal, ChangePermissions, \
+     ModifyPortalContent, View
+from Products.CMFCore.utils import UniqueObject
+from OFS.Folder import Folder
+
+from zLOG import LOG, DEBUG
+
+
+log_key = 'RemoteControllerTool'
+
+
+class RemoteControllerTool(UniqueObject, Folder):
+    """
+    """
+
+    id = 'portal_remote_controller'
+    meta_type = 'CPS Remote Controller Tool'
+    security = ClassSecurityInfo()
+
+
+    security.declareProtected(ChangePermissions, 'checkRoles')
+    def getRoles(self, username, REQUEST=None, **kw):
+        """Return the roles of the given user.
+        """
+        membersDirectory = self.portal_directories.members
+        entry = membersDirectory.getEntry(username)
+        roles = entry['roles']
+        return roles
+
+
+    security.declareProtected(ChangePermissions, 'getLocalRoles')
+    def getLocalRoles(self, username, rpath, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        local_roles_struct = proxy.getCPSLocalRoles()
+        LOG(log_key, DEBUG, "local_roles = %s" % str(local_roles_struct[0]))
+        user_local_roles = local_roles_struct[0]
+        user_key = 'user:%s' % username
+        if user_local_roles.get(user_key):
+            return user_local_roles[user_key][0]['roles']
+        else:
+            return []
+
+
+    security.declareProtected(ChangePermissions, 'checkPermission')
+    def checkPermission(self, rpath, permission, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        # checkPermission returns True if allowed and None otherwise, which is
+        # not consistent.
+        allowed = self.portal_membership.checkPermission(permission, proxy)
+        return allowed == True
+
+
+    security.declareProtected(View, 'listContent')
+    def listContent(self, rpath, REQUEST=None, **kw):
+        """Return the list of document contained in the document specified by
+        the given relative path.
+
+        rpath is of the form "workspaces" or "workspaces/folder1".
+        """
+        proxy = self.restrictedTraverse(rpath)
+        portal = self.portal_url.getPortalObject()
+        brains = portal.search(query={'cps_filter_sets': 'searchable'},
+                               folder_prefix=rpath)
+        objects_paths = [x.getPath()[len(portal.getBaseUrl()):]
+                         for x in brains]
+        return objects_paths
+
+
+    security.declareProtected(View, 'getDocumentState')
+    def getDocumentState(self, rpath, REQUEST=None, **kw):
+        """Return the workflow state of the document specified by the given
+        relative path.
+
+        rpath is of the form "workspaces/doc1" or "sections/doc2".
+        """
+        proxy = self.restrictedTraverse(rpath)
+        state = proxy.getContentInfo(proxy=proxy, level=0)['review_state']
+        return state
+
+
+    security.declareProtected(View, 'getDocumentHistory')
+    def getDocumentHistory(self, rpath, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        history = proxy.getContentInfo(proxy=proxy, level=3)['history']
+        LOG(log_key, DEBUG, "history = %s" % history)
+        # A simplified value of the history so that it can be transported over
+        # XML-RPC.
+        history_simplified = {}
+        for event in history:
+            history_simplified[event['action']] = event['time_str']
+        LOG(log_key, DEBUG, "history_simplified = %s" % history_simplified)
+        return history_simplified
+
+
+    security.declareProtected(View, 'getDocumentState')
+    def getDocumentState(self, rpath, REQUEST=None, **kw):
+        """Return the workflow state of the document specified by the given
+        relative path.
+
+        rpath is of the form "workspaces/doc1" or "sections/doc2".
+        """
+        proxy = self.restrictedTraverse(rpath)
+        state = proxy.getContentInfo(proxy=proxy, level=0)['review_state']
+        return state
+
+
+    security.declareProtected(View, 'isDocumentLocked')
+    def isDocumentLocked(self, rpath, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        return proxy.wl_isLocked()
+
+
+##     security.declareProtected(ModifyPortalContent, 'lockDocument')
+##     def lockDocument(self, rpath, REQUEST=None, **kw):
+##         """Lock the document and return the associated lock token or False if
+##         some problem arose.
+##         """
+##         proxy = self.restrictedTraverse(rpath)
+##         if proxy.wl_isLocked():
+##             return False
+##         lock = proxy.wl_getLock(token)
+
+
+##     security.declareProtected(ModifyPortalContent, 'unLockDocument')
+##     def unLockDocument(self, rpath, lock, REQUEST=None, **kw):
+##         """Un-lock the document.
+##         """
+##         proxy = self.restrictedTraverse(rpath)
+##         if proxy.wl_isLocked():
+##             return False
+##         lock = self.wl_getLock(token)
+
+
+    security.declareProtected(ModifyPortalContent, 'unpublishDocument')
+    def unpublishDocument(self, rpath, REQUEST=None, **kw):
+        """Unpublish the document specify by the given relative path.
+
+        rpath is of the form "sections/doc1" or "sections/folder/doc2".
+        """
+        wftool = self.portal_workflow
+        proxy = self.restrictedTraverse(rpath)
+        context = proxy
+        workflow_action = 'unpublish'
+        comments = "Un-publishing done through the Remote Controller"
+        allowed_transitions = wftool.getAllowedPublishingTransitions(context)
+        LOG(log_key, DEBUG, "allowed_transitions = %s" % str(allowed_transitions))
+        wftool.doActionFor(context, workflow_action, comment=comments)
+
+
+    security.declareProtected(ModifyPortalContent, 'changeDocumentOrder')
+    def changeDocumentOrder(self, rpath, step, REQUEST=None, **kw):
+        """Return XXX.
+
+        step must be a non-zero integer.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        id = proxy.getId()
+        context = proxy.aq_parent
+        newpos = context.get_object_position(id) + step
+        context.move_object_to_position(id, newpos)
+
+
+    security.declareProtected(ModifyPortalContent, 'get')
+    def createDocument(self, document_title, type_name, folder_rpath,
+                       data_dict={}, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        folder_proxy = self.restrictedTraverse(folder_rpath)
+        id = folder_proxy.computeId(compute_from=document_title)
+        folder_proxy.invokeFactory(type_name, id)
+        doc_proxy = getattr(folder_proxy, id)
+        doc = doc_proxy.getEditableContent()
+        doc.edit(Title=document_title)
+
+
+    security.declareProtected(ModifyPortalContent, 'get')
+    def editDocument(self, rpath, data_dict={}, REQUEST=None, **kw):
+        """Return XXX.
+        """
+        proxy = self.restrictedTraverse(rpath)
+        doc = proxy.getEditableContent()
+        doc.edit(data_dict)
+
+
+InitializeClass(RemoteControllerTool)
