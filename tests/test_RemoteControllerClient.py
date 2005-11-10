6dd6449762fefd0778a1d43ef3dcf92df97122c2
tziade <devnull@localhost>
2005-11-10 16:34:34 +0000

    fixed the test (the first one was not doing the right test)

--- tests/test_RemoteControllerClient.py
+++ tests/test_RemoteControllerClient.py
@@ -155,11 +155,14 @@ class RemoteControllerClientTC(CPSRemoteControllerTestCase.CPSRemoteControllerTe
 
         self.assertEquals(content, wanted)
 
-        content = [DateTime('01/01/1971')]
+        content = [[DateTime('01/01/1971')]]
         content = map(rd._controlArg, content)
         content = dumps(tuple(content))
-        wanted = ('<params>\n<param>\n<value><dateTime.iso8601>19710101T00:00:00'
-                  '</dateTime.iso8601></value>\n</param>\n</params>\n')
+
+        wanted = ('<params>\n<param>\n<value><array><data>\n<value>'
+                  '<dateTime.iso8601>19710101T00:00:00</dateTime.is'
+                  'o8601></value>\n</data></array></value>\n'
+                  '</param>\n</params>\n')
 
         self.assertEquals(content, wanted)
 
