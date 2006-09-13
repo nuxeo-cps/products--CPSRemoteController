9e23f3b097f87712955b8dc69501655db6966713
tziade <devnull@localhost>
2006-09-13 14:36:32 +0000

    tipo

--- RemoteControllerTool.py
+++ RemoteControllerTool.py
@@ -49,7 +49,7 @@ from Products.CPSCore.permissions import ViewArchivedRevisions
 from Products.CPSCore.EventServiceTool import getEventService
 from Products.CPSUtil.id import generateFileName
 from Products.CPSUtil.integration import getProductVersion
-from Products.CPSUtils.xmlrpc import toLatin9
+from Products.CPSUtil.xmlrpc import toLatin9
 
 from Products.CPSRemoteController.utils import unMarshallDocument
 from Products.CPSRemoteController.interfaces import IRemoteControllerTool
